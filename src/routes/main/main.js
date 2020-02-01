1fb6a0888f9aa3cc7b9cb22088a5bd13672d4807
Anna Kholod <uashop89@gmail.com>
2020-02-01 20:21:53 +0200

    added 3 work

--- src/routes/main/main.js
+++
@@ -1,7 +0,0 @@
-const mainRoute = (request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write("<h1>Main page</h1>");
-  response.end();
-};
-
-module.exports = mainRoute;
